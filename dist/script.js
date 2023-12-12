b960dcaeb8b63eff558bafab3fc0fa93f361d9b9
Fabio Privitera <fabio_privitera@yahoo.it>
2023-12-12 15:49:17 +0200

    fixes + gsap on section4

--- dist/script.js
+++ dist/script.js
@@ -1,7 +1,7 @@
 // Import ScrollMagic and GSAP
 // import 'scrollmagic/scrollmagic/uncompressed/plugins/debug.addIndicators';
 import ScrollMagic from 'scrollmagic';
-import { Tween } from 'gsap';
+import gsap from 'gsap';
 console.log('SUCA');
 
 var controller = new ScrollMagic.Controller();
@@ -47,3 +47,45 @@ const scene = new ScrollMagic.Scene({
   })
   .addTo(controller);
 console.log(document.getElementsByClassName('trigger-element'));
+
+//GSAP animation for bordercircles
+
+console.log(gsap);
+const timeline = gsap.timeline({ yoyo: true });
+timeline.to('.section4_container-images-circle', {
+  duration: 2,
+  opacity: 0,
+  transform: 'translate(-50%, -50%) scale(.5)',
+  ease: 'bounce',
+  stagger: 0.25,
+});
+timeline.to('.section4_container-images-border', {
+  duration: 0.5,
+  strokeDashoffset: 0,
+  ease: 'power2.inOut',
+  stagger: 0.5,
+});
+timeline.to(
+  '.section4_container-images-border',
+  {
+    duration: 2,
+    strokeDashoffset: -301.592,
+    stagger: 1,
+  },
+  '<'
+);
+timeline.from(
+  '.section4_container-images-circle',
+  {
+    duration: 2,
+    opacity: 1,
+    transform: 'translate(-50%, -50%) scale(1.5)',
+    ease: 'bounce',
+    stagger: 0.25,
+  },
+  '-=3.5'
+);
+
+timeline.repeat(-1);
+timeline.play();
+
